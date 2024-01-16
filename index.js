9494570fa049350a62c7bdef869cbd38971fb02c
sheikh-mamun <abalmamun2515@gmail.com>
2024-01-16 19:13:51 +0600

    fisrt

---
+++ index.js
@@ -0,0 +1,32 @@
+// document.write(5+6+ "<br>")
+// document.write(5+6)
+
+// var saltPrice=(10, 15, 16);
+// document.write(saltPrice)
+console.log(8+8 + "<br>");
+console.log(9+9 + "<br>");
+console.log(9+5 + "<br>");
+// document.write("text" + "<br>") 
+var country =("bangladesh");
+// document.write(country);
+
+var y =("our country name is bangladesh" + "<br>")
+console.log(y);
+ 
+
+document.write("name:" + y)
+
+var x = 5
+var y = 7
+
+// document .write(x+y)
+
+var name =["Bangladesh", "India", "nepal"]
+document.write(name)
+ 
+
+
+
+
+
+
