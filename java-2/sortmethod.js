c86b99050879cf0e7ed1468a1d388b082be45bf8
sheikh-mamun <abalmamun2515@gmail.com>
2024-01-20 13:21:04 +0600

    new

---
+++ java-2/sortmethod.js
@@ -0,0 +1,85 @@
+/*var a =['rafia', 'foisal', 'karim'];
+a.push('jamal');
+a.push('junayed');
+console.log(a);
+
+
+
+var a =['rafia', 'foisal', 'karim', 'abdullah', 'balam'];
+a.sort();
+console.log(a);
+*/
+
+// object
+
+// var yamaha ={
+//   price: 200,
+//   color: 'black',
+//   size: 'large',
+//   brand: 'japani',
+//   feature: 2023,
+//   place:['dhaka', 'barisal', 'komilla', 'rajshahi', 'sirajgonj', 'rangpur']
+// };
+
+// console.log(yamaha['place']);
+
+// new object
+
+// var yamaha ={price: 200, color:'black', brand:"japani", feature:2023};
+
+var yamaha ={
+  price: 200,
+  color:'black',
+  brand:"japani",
+  feature:2023,
+  size:'big',
+  beautiful:false,
+  design:['small', 'big', 'midium','large'],
+  nextobj:{
+    color:'blue',
+    size:'small',
+  }
+    
+};
+
+// Object.freeze(yamaha);
+// delete yamaha.size;
+
+
+// console.log(nextobja);
+
+// let name = 'raisha';
+// let nam ='karim';
+
+// console.log(nam,name);
+
+// const price =20;
+// const name ='raju';
+
+// console.log(price);
+
+// Mathmatical term
+
+const eggPrice = 60;
+const saltPrice = 40;
+// const total = eggPrice + saltPrice;
+
+// console.log(eggPrice + saltPrice);
+
+const income = 4000;
+const cost = 1000;
+result = income / cost;
+console.log(result);
+
+let firstName = 'karim';
+let lastName = "jaman";
+let profession = 'teacher';
+
+console.log( "biography:" firstName + " " + lastName + " " + profession);
+
+
+
+
+
+
+
