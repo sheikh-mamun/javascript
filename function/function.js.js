3e4b65ef850fe4eea1ef5200f820bb723f0674ab
sheikh-mamun <abalmamun2515@gmail.com>
2024-01-23 09:28:38 +0600

    new

---
+++ function/function.js.js
@@ -0,0 +1,36 @@
+// function add(num1,num2) {
+//   console.log(num1+num2)
+// }
+// add(2,3);
+
+// function myFunc (name,country) {
+//   console.log('my name is ' + name + ' i am from '+ country )
+// }
+// myFunc('rahim','bangladesh');
+// myFunc('karim','india');
+// myFunc('foisal','pakistan');
+
+// function add (a,b){
+// console.log(a/b);
+// }
+// add (12,6);
+
+
+// function addNumber ( num1,num2) {
+//   let total=(num1+ num2);
+//   return total;
+ 
+
+// }
+//  let total = addNumber(10,20);
+// console.log (total);
+
+// function now (money) {
+//  let eggprice = 5;
+//  let total =(money/eggprice);
+//  return total;
+// }
+// let fixed= now (50);
+// console.log(fixed);
+
+
--- java-2/sortmethod.js
+++ java-2/sortmethod.js
@@ -27,20 +27,20 @@ console.log(a);
 
 // var yamaha ={price: 200, color:'black', brand:"japani", feature:2023};
 
-var yamaha ={
-  price: 200,
-  color:'black',
-  brand:"japani",
-  feature:2023,
-  size:'big',
-  beautiful:false,
-  design:['small', 'big', 'midium','large'],
-  nextobj:{
-    color:'blue',
-    size:'small',
-  }
+// var yamaha ={
+//   price: 200,
+//   color:'black',
+//   brand:"japani",
+//   feature:2023,
+//   size:'big',
+//   beautiful:false,
+//   design:['small', 'big', 'midium','large'],
+//   nextobj:{
+//     color:'blue',
+//     size:'small',
+//   }
     
-};
+// };
 
 // Object.freeze(yamaha);
 // delete yamaha.size;
@@ -66,16 +66,176 @@ const saltPrice = 40;
 
 // console.log(eggPrice + saltPrice);
 
-const income = 4000;
-const cost = 1000;
-result = income / cost;
-console.log(result);
+// const income = 4000;
+// const cost = 1000;
+// result = income / cost;
+// console.log(result);
 
-let firstName = 'karim';
-let lastName = "jaman";
-let profession = 'teacher';
+// let firstName = 'karim';
+// let lastName = "jaman";
+// let profession = 'teacher';
 
-console.log( "biography:" firstName + " " + lastName + " " + profession);
+// console.log( "biography:" firstName + " " + lastName + " " + profession);
+
+
+// / console.log(num1);
+
+
+// shortHand
+// let price = 50;
+// price += 20;
+// console.log(price);
+
+
+// let price = 50;
+// price -= 20;
+// console.log(price);
+
+// let price = 50;
+// price ++;
+// console.log(price);
+
+// let price = 50;
+// price *= 5;
+// console.log(price);
+
+
+// let price = 100;
+// price /= 20;
+// console.log(price);
+
+// conditional
+
+// let  burgerPrice = 100;
+// let  myBudget = 100;
+// if(burgerPrice < myBudget) {
+//   console.log('burger khabo');
+// }
+// else {
+//   console.log('khabo na');
+// }
+
+// let score = 100;
+
+// if(score >= 80){
+// console.log('A+');
+// }
+
+// else if(score >= 70){
+// console.log('A');
+// }
+
+// else if(score >= 60){
+//   console.log('A-');
+//   }
+
+//   else if(score >= 60){
+//     console.log('B');
+//     }
+
+//     else if(score >= 50){
+//       console.log('C');
+//       }
+
+//       else if(score >= 40){
+//         console.log('D');
+//         }
+
+//         else if(score < 40 ){
+//           console.log('Fail');
+//           }
+    
+  // let isMatricPass = true;
+  // let income = 20000;
+  // let iphone = 2;
+
+  // if(isMatricPass == true && income >= 20000 || iphone >= 1){
+  // console.log('yes, somporko korbo');
+  // }
+
+  // else {
+  //   console.log("no, somporko korbo na")
+  // }
+
+  // nested condition//
+
+//   let a = 6;
+//   let b = 5;
+//   let c = 4;
+  
+//   if(a < b){
+//     if(b < c){
+//       console.log('true');
+//     }
+// }
+
+//   else{
+//     console.log('no true');
+//   }
+
+// loop
+
+// let score = 0;
+// while(score < 10){
+//   console.log('halow world');
+//   score++;
+// }
+
+// let count = 10;
+// while(count <= 1){
+//   console.log(count);
+//   count--;
+
+// }
+
+  
+// function myFunc (name , country) {
+//   console.log("my name is " + name +  " i am form " + country);
+// }
+
+// myFunc("rahim", 'bangladesh');
+// myFunc("foisal", 'india');
+// myFunc("karim", 'china');
+
+// function add (a,b) {
+//   console.log(a+b);
+// }
+// add(3,7);
+
+
+// function addNumber (num1, num2) {
+//    let total = num1+num2;
+//    return total;
+// }
+
+//  let total = addNumber(4,8);
+//  console.log(total);
+
+
+ function now (money) {
+  let cost = 30;
+  let total = money/cost;
+  return total 
+
+ }
+
+ let result= now(60);
+ console.log(result);
+
+
+
+
+
+    
+
+
+
+
+   
+
+      
+
+
 
 
 
